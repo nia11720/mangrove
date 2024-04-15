dde39c87f27b7d892162a5db33839371b35d110c
nia <mangrove0720@outlook.com>
2024-04-15 15:53:34 +0800

    add bluprint.bing

--- src/mangrove/__init__.py
+++ src/mangrove/__init__.py
@@ -5,13 +5,14 @@ app = Flask(__name__)
 with app.app_context():
     import bilibili
     import youtube
-    from .blueprint import test, cpolar
+    from .blueprint import test, cpolar, bing
 
     app.register_flask(bilibili.app, url_prefix="/b")
     app.register_flask(youtube.app, url_prefix="/ytb")
 
     app.register_blueprint(test.bp, url_prefix="/test")
     app.register_blueprint(cpolar.bp, url_prefix="/cpolar")
+    app.register_blueprint(bing.bp, url_prefix="/bing")
 
 
 @app.get("/")
